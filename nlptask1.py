e18bbbf645b5287000ceeebe943091fcbe5c5b3b
SnehaKumar295 <58648260+SnehaKumar295@users.noreply.github.com>
2019-12-08 11:25:18 +0530

    Add files via upload

---
+++ nlptask1.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+a="Shakespeare gave the world best of theatrics"
+b='''William Shakespeare (bapt. 26 April 1564 – 23 April 1616) was an English poet, playwright, and actor, widely regarded as the greatest writer in the English language and the world's greatest dramatist. He is often called England's national poet and the "Bard of Avon" (or simply "the Bard").
+His extant works, including collaborations, consist of some 39 plays,[c] 154 sonnets, two long narrative poems, and a few other verses, some of uncertain authorship. His plays have been translated into every major living language and are performed more often than those of any other playwright.[7]'''
+b=b+a;
+print(b)
+k="दीपावली पर कविता- सूर्य का तेज, सूर्य की रोशनी, सूर्य का वजूद, दुनिया जानती है।"
+print(k)
